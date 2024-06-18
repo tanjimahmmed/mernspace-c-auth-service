4181d7370f3995f75d9d638f0496539348de31e5
Md Tanjim Ahmmed <44721685+tanjimahmmed@users.noreply.github.com>
2024-06-18 18:00:14 +0600

    add logger

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,21 @@
 import app from "./app";
 import { Config } from "./config"
+import logger from "./config/logger";
 
 const startServer = () => {
     const PORT = Config.PORT;
 
     try {
-        app.listen(PORT, () => console.log(`Listening on port ${PORT}`))
-    } catch (err) {
-        console.error(err);
-        process.exit(1);
+        throw new Error("Something went wromg");
+        app.listen(PORT, () => logger.info(`Listening on port ${PORT}`, {test: 1}))
+    } catch (err: unknown) {
+        if(err instanceof Error){
+            logger.error(err.message);
+            setTimeout(() => {
+                process.exit(1);
+            }, 1000)
+            
+        }
     }
     
 }
