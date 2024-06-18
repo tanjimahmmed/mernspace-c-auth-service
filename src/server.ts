6d437ab67186f8057af6b62976179f4084f53087
Md Tanjim Ahmmed <44721685+tanjimahmmed@users.noreply.github.com>
2024-06-18 14:47:45 +0600

    add express js

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,16 @@
-import { Config } from "./config";
+import app from "./app";
+import { Config } from "./config"
 
+const startServer = () => {
+    const PORT = Config.PORT;
 
-console.log('PORT', Config.NODE_ENV);
+    try {
+        app.listen(PORT, () => console.log(`Listening on port ${PORT}`))
+    } catch (err) {
+        console.error(err);
+        process.exit(1);
+    }
+    
+}
+
+startServer();
