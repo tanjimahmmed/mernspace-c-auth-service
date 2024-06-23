ff46cdae215f08cbf50b48afd07f19213378d189
Md Tanjim Ahmmed <44721685+tanjimahmmed@users.noreply.github.com>
2024-06-23 14:28:50 +0600

    email require validation

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -2,6 +2,7 @@ import { NextFunction, Response } from "express";
 import { RegisterUserRequest } from "../types";
 import { UserService } from "../services/UserService";
 import {Logger} from "winston";
+import { validationResult } from "express-validator";
 
 
 export class AuthController {
@@ -9,8 +10,14 @@ export class AuthController {
 
     async register(req: RegisterUserRequest, res: Response, next: NextFunction) {
         
-        const {firstName, lastName, email, password} = req.body;
+        // validation
+        const result = validationResult(req);
+        if (!result.isEmpty()) {
+            return res.status(400).json({errors: result.array()})
+        }
 
+        const {firstName, lastName, email, password} = req.body;
+        
         this.logger.debug("New request to register a user", {
             firstName,
             lastName,
