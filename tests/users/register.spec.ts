f344ec1f270ee3174a8d14812049bb4358d40e02
Md Tanjim Ahmmed <44721685+tanjimahmmed@users.noreply.github.com>
2024-06-23 14:40:58 +0600

    add email sanitization

--- tests/users/register.spec.ts
+++ tests/users/register.spec.ts
@@ -179,4 +179,27 @@ describe("POST /auth/register", () => {
             expect(users).toHaveLength(0)
          })
     })
+
+    describe("Fields are not in proper format", () => {
+        it("should trim the email field", async () => {
+            // Arrange 
+            const userData = {
+                firstName: "Tanjim",
+                lastName: "Jimmiy",
+                email: " tanjim@mern.space ",
+                password: "secret"
+            }
+            
+            // Act
+            await request(app)
+            .post('/auth/register')
+            .send(userData)
+
+            // Assert
+            const userRepository = connection.getRepository(User)
+            const users = await userRepository.find();
+            const user = users[0];
+            expect(user.email).toBe("tanjim@mern.space")
+        })
+    })
 })
