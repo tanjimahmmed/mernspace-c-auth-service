334f02f3b2e7124276cb578fca79203190cce6b5
Md Tanjim Ahmmed <44721685+tanjimahmmed@users.noreply.github.com>
2024-07-02 18:34:52 +0600

    setup coverage

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,12 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  verbose: true
+  verbose: true,
+  collectCoverage: true,
+  coverageProvider: "v8",
+  collectCoverageFrom: [
+    "src/**/*.ts",
+    "!tests/**",
+    "!**/node_modules/**"
+  ]
 };
